cddb6764b7e9745717fdf1ed1c607826e153d0b7
Kyle Phillips <kyle@object9.com>
2015-02-23 09:33:25 -0500

    Header revisions

--- wp-content/themes/base/header.php
+++ wp-content/themes/base/header.php
@@ -5,46 +5,23 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	
 	<?php if (is_search()) { ?>
-	   <meta name="robots" content="noindex, nofollow" /> 
+	<meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
 
-	<title>
-		   <?php
-		      if (function_exists('is_tag') && is_tag()) {
-		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-		      elseif (is_archive()) {
-		         wp_title(''); echo ' Archive - '; }
-		      elseif (is_search()) {
-		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
-		         wp_title(''); echo ' - '; }
-		      elseif (is_404()) {
-		         echo 'Not Found - '; }
-		      if (is_home()) {
-		         bloginfo('name'); echo ' - '; bloginfo('description'); }
-		      else {
-		          bloginfo('name'); }
-		      if ($paged>1) {
-		         echo ' - page '. $paged; }
-		   ?>
-	</title>
+	<title><?php wp_title('-'); ?></title>
 	
-	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v=1.0">
 
-	<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
+	<!--[if IE 8]>
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/assets/css/ie.css" />
+	<![endif]-->
+
+    <!--[if lt IE 9]>
+    	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 
 	<?php wp_head(); ?>
 </head>
-
-<body <?php body_class(); ?>>
-	
-	<div id="page-wrap">
-
-		<div id="header">
-			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-			<div class="description"><?php bloginfo('description'); ?></div>
-		</div>
