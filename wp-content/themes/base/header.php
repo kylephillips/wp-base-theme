7edb9469d0d12720f3f2220a4381868329af4bec
Kyle Phillips <kyle@object9.com>
2015-10-02 10:35:54 -0400

    Favicon replaced with site icon.

--- wp-content/themes/base/header.php
+++ wp-content/themes/base/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	
@@ -10,7 +9,7 @@
 
 	<title><?php wp_title('-'); ?></title>
 	
-	<link rel="icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
+	<?php wp_site_icon(); ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v=1.0">
