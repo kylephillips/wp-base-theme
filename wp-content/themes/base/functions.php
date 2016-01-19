13040e459e78f97e0f0e74cc25cb590b6b175c48
Austin Bloom <austin@object9.com>
2016-01-19 17:16:22 -0500

    tom update

--- wp-content/themes/base/functions.php
+++ wp-content/themes/base/functions.php
@@ -42,4 +42,80 @@ function cc_mime_types($mimes)
 	return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+/**
+ * Begin required plugins
+ */
+ 
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
+ 
+add_action( 'tgmpa_register', 'mytheme_require_plugins' );
+ 
+function mytheme_require_plugins() {
+ 
+    $plugins = array(
+		   // Commenting out these two since they require license keys
+		    /*array(
+		        'name'               => 'Advanced Custom Fields Pro',
+		        'slug'               => 'advanced-custom-fields-pro',
+		        'source'             => get_stylesheet_directory() . '/lib/plugins/advanced-custom-fields-pro.zip', // The "internal" source of the plugin
+		        'required'           => true, // this plugin is required
+		        'external_url'       => 'https://wordpress.org/plugins/advanced-custom-fields/', // page of my plugin
+		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
+		    ),
+		    array(
+		        'name'               => 'Gravity Forms',
+		        'slug'               => 'gravity-forms',
+		        'source'             => get_stylesheet_directory() . '/lib/plugins/gravityforms_1.9.15.18.zip', // The "internal" source of the plugin
+		        'required'           => true, // this plugin is required
+		        'external_url'       => 'http://www.gravityforms.com//', // page of my plugin
+		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
+		    ),*/
+		    array(
+		        'name'               => 'Nested Pages',
+		        'slug'               => 'wp-nested-pages',
+		        'source'             => 'http://downloads.wordpress.org/plugin/wp-nested-pages.1.5.4.zip',
+		        'required'           => true, // this plugin is required
+		        'external_url'       => 'https://wordpress.org/plugins/wp-nested-pages/', // page of my plugin
+		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
+		    ),
+		    array(
+		        'name'               => 'Google Analytics',
+		        'slug'               => 'googleanalytics',
+		        'source'             => 'http://downloads.wordpress.org/plugin/googleanalytics.zip',
+		        'required'           => true, // this plugin is required
+		        'external_url'       => 'https://wordpress.org/plugins/googleanalytics/', // page of my plugin
+		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
+		    ),
+		    array(
+		        'name'               => 'Post Thumbnail Editor',
+		        'slug'               => 'post-thumbnail-editor',
+		        'source'             => 'http://downloads.wordpress.org/plugin/post-thumbnail-editor.zip',
+		        'required'           => true, // this plugin is required
+		        'external_url'       => 'https://wordpress.org/plugins/post-thumbnail-editor/', // page of my plugin
+		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
+		    ),
+		    array(
+		        'name'               => 'Yoast SEO',
+		        'slug'               => 'wordpress-seo',
+		        'source'             => 'http://downloads.wordpress.org/plugin/wordpress-seo.3.0.7.zip',
+		        'required'           => true, // this plugin is required
+		        'external_url'       => 'https://wordpress.org/plugins/wordpress-seo/', // page of my plugin
+		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
+		    )
+		);
+    $config = array(
+        'id'           => 'tgmpa', // your unique TGMPA ID
+        'default_path' => '', // default absolute path
+        'menu'         => 'tgmpa-install-required-plugins', // menu slug
+        'has_notices'  => true, // Show admin notices
+        'dismissable'  => false, // the notices are NOT dismissable
+        'dismiss_msg'  => 'Please install these recommended plugins', // this message will be output at top of nag
+        'is_automatic' => true, // automatically activate plugins after installation
+        'message'      => '<!--Hey there.-->'// message to output right before the plugins table
+    );
+ 
+    tgmpa( $plugins, $config );
+ 
+}
 ?>
