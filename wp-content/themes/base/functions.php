ab1b57885e4e6411a1e940f09d39e73c12faebcb
Kyle Phillips <kyle@object9.com>
2016-01-26 12:53:05 -0500

    Formatting of TGM plugin activation. Class moved to lib directory.

--- wp-content/themes/base/functions.php
+++ wp-content/themes/base/functions.php
@@ -44,61 +44,40 @@ function cc_mime_types($mimes)
 add_filter('upload_mimes', 'cc_mime_types');
 
 /**
- * Begin required plugins
- */
- 
-require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
- 
-add_action( 'tgmpa_register', 'mytheme_require_plugins' );
- 
-function mytheme_require_plugins() {
- 
-    $plugins = array(
-		    array(
-		        'name'               => 'Nested Pages',
-		        'slug'               => 'wp-nested-pages',
-		        'source'             => 'http://downloads.wordpress.org/plugin/wp-nested-pages.1.5.4.zip',
-		        'required'           => true, // this plugin is required
-		        'external_url'       => 'https://wordpress.org/plugins/wp-nested-pages/', // page of my plugin
-		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
-		    ),
-		    array(
-		        'name'               => 'Google Analytics',
-		        'slug'               => 'googleanalytics',
-		        'source'             => 'http://downloads.wordpress.org/plugin/googleanalytics.zip',
-		        'required'           => true, // this plugin is required
-		        'external_url'       => 'https://wordpress.org/plugins/googleanalytics/', // page of my plugin
-		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
-		    ),
-		    array(
-		        'name'               => 'Post Thumbnail Editor',
-		        'slug'               => 'post-thumbnail-editor',
-		        'source'             => 'http://downloads.wordpress.org/plugin/post-thumbnail-editor.zip',
-		        'required'           => true, // this plugin is required
-		        'external_url'       => 'https://wordpress.org/plugins/post-thumbnail-editor/', // page of my plugin
-		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
-		    ),
-		    array(
-		        'name'               => 'Yoast SEO',
-		        'slug'               => 'wordpress-seo',
-		        'source'             => 'http://downloads.wordpress.org/plugin/wordpress-seo.3.0.7.zip',
-		        'required'           => true, // this plugin is required
-		        'external_url'       => 'https://wordpress.org/plugins/wordpress-seo/', // page of my plugin
-		        'force_deactivation' => false, // deactivate this plugin when the user switches to another theme
-		    )
-		);
-    $config = array(
-        'id'           => 'tgmpa', // your unique TGMPA ID
-        'default_path' => '', // default absolute path
-        'menu'         => 'tgmpa-install-required-plugins', // menu slug
-        'has_notices'  => true, // Show admin notices
-        'dismissable'  => false, // the notices are NOT dismissable
-        'dismiss_msg'  => 'Please install these recommended plugins. Also, please install Gravity Forms Pro and Advanced Custom Fields Pro (license required)', // this message will be output at top of nag
-        'is_automatic' => true, // automatically activate plugins after installation
-        'message'      => '<!--Hey there.-->'// message to output right before the plugins table
-    );
- 
-    tgmpa( $plugins, $config );
- 
+* Required plugins
+*/ 
+require_once dirname( __FILE__ ) . '/lib/class-tgm-plugin-activation.php';
+function mytheme_require_plugins()
+{ 
+	$plugins = array(
+		array(
+			'name'					=> 'Nested Pages',
+			'slug'					=> 'wp-nested-pages',
+			'source'				=> 'http://downloads.wordpress.org/plugin/wp-nested-pages.1.5.4.zip',
+			'required'				=> true, // this plugin is required
+			'external_url'			=> 'https://wordpress.org/plugins/wp-nested-pages/', // page of my plugin
+			'force_deactivation'	=> false, // deactivate this plugin when the user switches to another theme
+		),
+		array(
+			'name'					=> 'Yoast SEO',
+			'slug'					=> 'wordpress-seo',
+			'source'				=> 'http://downloads.wordpress.org/plugin/wordpress-seo.3.0.7.zip',
+			'required'				=> true, // this plugin is required
+			'external_url'			=> 'https://wordpress.org/plugins/wordpress-seo/', // page of my plugin
+			'force_deactivation'	=> false, // deactivate this plugin when the user switches to another theme
+		)
+	);
+	$config = array(
+		'id'						=> 'tgmpa', // your unique TGMPA ID
+		'default_path'				=> '', // default absolute path
+		'menu' 						=> 'tgmpa-install-required-plugins', // menu slug
+		'has_notices'				=> true, // Show admin notices
+		'dismissable'				=> true, // the notices are NOT dismissable
+		'dismiss_msg'				=> 'Please install these recommended plugins. Also, please install Gravity Forms Pro and Advanced Custom Fields Pro (license required)', // this message will be output at top of nag
+		'is_automatic'				=> true, // automatically activate plugins after installation
+		'message'					=> '<!--Hey there.-->'// message to output right before the plugins table
+	);
+	tgmpa( $plugins, $config );
 }
+add_action( 'tgmpa_register', 'mytheme_require_plugins' );
 ?>
