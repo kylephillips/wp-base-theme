44ca82d051af38f699d51afb18d1c4cef5b22874
Kyle Phillips <kyle@object9.com>
2017-06-05 09:26:16 -0400

    References jQuery CDN with https

--- wp-content/plugins/base/app/Dependencies.php
+++ wp-content/plugins/base/app/Dependencies.php
@@ -17,7 +17,7 @@ class Dependencies
 	public function jquery()
 	{
 		wp_deregister_script('jquery');
-		wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"), false);
+		wp_register_script('jquery', ("https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"), false);
 		wp_enqueue_script('jquery');
 	}
 
