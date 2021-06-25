535052f708e3558dccd102bbb4a9d44583b69ffd
Kyle Phillips <kyle@object9.com>
2021-06-25 14:44:04 -0400

    Corrects namespacing issue

--- wp-content/plugins/base/base.php
+++ wp-content/plugins/base/base.php
@@ -3,11 +3,11 @@
 Plugin Name: Base Theme Plugin
 Plugin URI: https://github.com/kylephillips/wp-base-theme
 Description: Various Items Required by the Base Theme
-Version: 1.0
+Version: 1.0.0
 Author: Kyle Phillips
 Author URI: https://github.com/kylephillips
 License: GPL
 */
-$loader = require_once 'vendor/autoload.php';
-require_once('app/Bootstrap.php');
+$loader = require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/app/Bootstrap.php');
 $base_plugin = new Base\Bootstrap;
