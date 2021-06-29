6c64620283b405176aaea5adc0421bdd9190c5b9
Kyle Phillips <kyle@object9.com>
2021-06-29 17:08:47 -0400

    Updates column styles to support container elements using inner columns

--- wp-content/themes/base/page.php
+++ wp-content/themes/base/page.php
@@ -3,10 +3,8 @@ get_header();
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
 
-<div class="container">
-	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
-</div><!-- .container -->
+<h2><?php the_title(); ?></h2>
+<?php the_content(); ?>
 
 <?php 
 endwhile; endif;
