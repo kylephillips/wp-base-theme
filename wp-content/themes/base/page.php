b3e9ede2e5dfe383cfd38b780e4ec228df429c84
Kyle Phillips <kyle@object9.com>
2023-03-23 16:18:50 -0400

    Removes title from page (for block pages)

--- wp-content/themes/base/page.php
+++ wp-content/themes/base/page.php
@@ -1,11 +1,6 @@
 <?php 
 get_header(); 
 if ( have_posts() ) : while ( have_posts() ) : the_post();
-?>
-
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
-
-<?php 
+the_content();
 endwhile; endif;
 get_footer();
