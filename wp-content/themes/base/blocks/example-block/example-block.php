208d6b0abdbe1fe8b39795a6dd21a2cc311f8e0a
Kyle Phillips <kylephillipsdesign@gmail.com>
2025-04-29 12:32:18 -0400

    Removes example block (example provided with modal block)

--- wp-content/themes/base/blocks/example-block/example-block.php
+++
@@ -1,6 +0,0 @@
-<?php
-$presenter = new \Base\Presenters\BlockPresenter($block);
-?>
-<div class="block-example <?php echo $presenter->css(); ?>" id="<?php echo $block['id']; ?>" <?php echo $presenter->styles(); ?>>
-	
-</div><!-- .block-example -->
